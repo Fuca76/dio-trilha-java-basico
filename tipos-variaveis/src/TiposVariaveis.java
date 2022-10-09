5f807f0e296801848a9dcfbb1f4068fdb7caf844
Fuca76 <taisocarvalhocampos@gmail.com>
2022-10-09 12:24:59 -0300

    adicionando conteúdo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,22 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Taís Carvalho";
+
+       double salarioMinimo = 2500;
+
+       short numeroCurto = 1;
+       int numeroNormal = numeroCurto;
+       short numeroCurto2 = (short)numeroNormal;
+
+      int numero = 5;
+      numero = 10;
+      System.out.print(numero);
+
+      final double VALOR DE PI =3,14;
+
+
     }
 }
